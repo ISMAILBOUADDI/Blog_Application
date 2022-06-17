bb42f924f1b77f114487fe4d27f6999e104a9e96
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2022-06-17 15:55:21 +0100

    upadte

--- backend/controllers/users/usersCtrl.js
+++ backend/controllers/users/usersCtrl.js
@@ -1,21 +1,21 @@
 const User = require('../../model/user/User');
 const expressAsyncHandler = require('express-async-handler');
 
-const userRegisterCtrl = async (req, res) => {
-//   console.log(req.body);
-//Check if user already exists
-const userExist = await User.findOne({ email: req?.body?.email });
-if(userExist) return res.status(400).json({ msg: 'User already exists' });
-    try {
-        const user = await User.create({
-            firstName:req?.body?.firstName,
-            lastName:req?.body?.lastName,
-            email:req?.body?.email,
-            password:req?.body?.password,
-        })
-        res.status(200).json(user);
-    } catch (error) {
-    res.json(error);
-    }
-}
+const userRegisterCtrl = expressAsyncHandler(async (req, res) => {
+    //   console.log(req.body);
+    //Check if user already exists
+    const userExist = await User.findOne({ email: req?.body?.email });
+    if(userExist) return res.status(400).json({ msg: 'User already exists' });
+        try {
+            const user = await User.create({
+                firstName:req?.body?.firstName,
+                lastName:req?.body?.lastName,
+                email:req?.body?.email,
+                password:req?.body?.password,
+            })
+            res.status(200).json(user);
+        } catch (error) {
+        res.json(error);
+        }
+    });
 module.exports = {userRegisterCtrl};
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dbConnect = require('./config/db/dbConnect');
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const {errorHandles} = require('./middlewares/error/errorHandles');
 const {userRegisterCtrl} = require('./controllers/users/usersCtrl');
 
 
@@ -14,5 +14,8 @@ dbConnect();
 app.use(express.json());
 //users Route
 app.use('/api/users', usersRoute);
-app.post('/api/users/register', userRegisterCtrl);
+
+//error 
+app.use(errorHandles);
+//server 
 app.listen(5000, console.log(`Server started on port ${PORT}`));
