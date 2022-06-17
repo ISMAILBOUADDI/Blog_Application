fea83059d62d352dac1e1f14ed3f89792035612f
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2022-06-17 14:36:07 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const dbConnect = require('./config/db/dbConnect');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const {userRegisterCtrl} = require('./controllers/users/usersCtrl');
  
 //server 
 dbConnect();
+//Middleware
+app.use(express.json());
+//Register
+app.post('/api/users/register', userRegisterCtrl);
 app.listen(5000, console.log(`Server started on port ${PORT}`));
