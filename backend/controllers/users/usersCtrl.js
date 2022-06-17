392018e34f65abd4fd44c341d1b746e0231472b6
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2022-06-17 14:44:54 +0100

    check if user exist

--- backend/controllers/users/usersCtrl.js
+++ backend/controllers/users/usersCtrl.js
@@ -2,7 +2,10 @@ const User = require('../../model/user/User');
 
 
 const userRegisterCtrl = async (req, res) => {
-  console.log(req.body);
+//   console.log(req.body);
+//Check if user already exists
+const userExist = await User.findOne({ email: req?.body?.email });
+if(userExist) return res.status(400).json({ msg: 'User already exists' });
     try {
         const user = await User.create({
             firstName:req?.body?.firstName,
