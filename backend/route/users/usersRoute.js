19e878ff68c50de48fdb48d533e124ce1429f13b
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2022-06-18 11:58:45 +0100

    update user route

--- backend/route/users/usersRoute.js
+++ backend/route/users/usersRoute.js
@@ -1,8 +1,18 @@
 const express = require('express');
+
+const {
+    userRegisterCtrl,
+    loginUserCtrl,
+    fetchUserCtrl,
+    deleteUserCtrl,
+    userDetailsCtrl
+} = require('../../controllers/users/usersCtrl');
+
 const router = express.Router();
-const {userRegisterCtrl} = require('../../controllers/users/usersCtrl');
 
 router.post('/register', userRegisterCtrl);
-
-
+router.post('/login', loginUserCtrl);
+router.get('/', fetchUserCtrl);
+router.delete('/:id', deleteUserCtrl);
+router.get('/:id', userDetailsCtrl);
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,7 @@ const dbConnect = require('./config/db/dbConnect');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const {errorHandles} = require('./middlewares/error/errorHandles');
-const {userRegisterCtrl} = require('./controllers/users/usersCtrl');
-
+const {notFound} = require('./middlewares/error/errorHandles');
 
 //import routes
 const usersRoute = require('./route/users/usersRoute');
@@ -16,6 +15,7 @@ app.use(express.json());
 app.use('/api/users', usersRoute);
 
 //error 
+app.use(notFound);
 app.use(errorHandles);
 //server 
 app.listen(5000, console.log(`Server started on port ${PORT}`));
