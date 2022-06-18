24c0579af3d438143f6789df1c084634f6de0214
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2022-06-18 12:39:34 +0100

    auth midddleware done ✔️

--- backend/middlewares/auth/authMiddleware.js
+++ backend/middlewares/auth/authMiddleware.js
@@ -1,6 +1,6 @@
 const expressAsyncHandler = require('express-async-handler');
 const jwt = require('jsonwebtoken');
-const User = require('../../models/user');
+const User = require('../../model/user/User');
 
 const authMiddleware = expressAsyncHandler(async (req, res, next) => {
    let token ;
@@ -10,10 +10,19 @@ const authMiddleware = expressAsyncHandler(async (req, res, next) => {
         token = req.headers.authorization.split(' ')[1];
         if(token){
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            //find the user by id
+            const user = await User.gindbyId(decoded.id).select('-password');
+            //attach the user to the request object
+            req.user = user;
+            next();
+        }else {
+            throw new Error('there is no token attached to the header');
         }
        } catch (error) {
-        
+         throw new Error('Not authorized token wxpired, login again');
        }
    }
 
-})
+});
+
+module.exports = authMiddleware;
