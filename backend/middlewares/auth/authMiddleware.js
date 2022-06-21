c3eeb18839211b07c3f65722408732a78a428d9d
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2022-06-21 22:52:49 +0100

    update

--- backend/middlewares/auth/authMiddleware.js
+++ backend/middlewares/auth/authMiddleware.js
@@ -8,7 +8,7 @@ const authMiddleware = expressAsyncHandler(async (req, res, next) => {
    if(req?.headers?.authorization?.startsWith('Bearer')){
        try {
         token = req.headers.authorization.split(' ')[1];
-        console.log(token);
+        // console.log(token);
         if(token){
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             //find the user by id
