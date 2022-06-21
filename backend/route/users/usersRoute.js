0993bf3eed1792486eb9b2317486b944607230fe
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2022-06-21 22:52:27 +0100

    user router updated

--- backend/route/users/usersRoute.js
+++ backend/route/users/usersRoute.js
@@ -10,6 +10,7 @@ const {
     updateUserProfileCtrl,
     updatePasswordCtrl,
     followingUserCtrl,
+    unfollowUserCtrl,
 } = require('../../controllers/users/usersCtrl');
 const authMiddleware = require('../../middlewares/auth/authMiddleware');
 
@@ -20,6 +21,7 @@ router.post('/login', loginUserCtrl);
 router.get('/', authMiddleware, fetchUserCtrl);
 router.put("/password", authMiddleware, updatePasswordCtrl);
 router.put("/follow",authMiddleware,followingUserCtrl);
+router.put("/unfollow",authMiddleware,unfollowUserCtrl);
 router.get("/profile/:id", authMiddleware, userProfileCtrl);
 router.put("/:id", authMiddleware, updateUserProfileCtrl);
 router.delete('/:id', deleteUserCtrl);
