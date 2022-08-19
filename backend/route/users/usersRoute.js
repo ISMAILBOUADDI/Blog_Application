56df11e2de2cd54db32815e156621ebc7d1817bf
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2022-08-19 21:34:23 +0100

    upload router

--- backend/route/users/usersRoute.js
+++ backend/route/users/usersRoute.js
@@ -12,6 +12,8 @@ const {
     followingUserCtrl,
     unfollowUserCtrl,
     blockUserCtrl,
+    unBlockUserCtrl,
+    profilePohotoUploadCtrl
 } = require('../../controllers/users/usersCtrl');
 const authMiddleware = require('../../middlewares/auth/authMiddleware');
 
@@ -19,11 +21,13 @@ const router = express.Router();
 
 router.post('/register', userRegisterCtrl);
 router.post('/login', loginUserCtrl);
+router.put('/profilephoto-upload', authMiddleware, profilePohotoUploadCtrl);
 router.get('/', authMiddleware, fetchUserCtrl);
 router.put("/password", authMiddleware, updatePasswordCtrl);
 router.put("/follow",authMiddleware,followingUserCtrl);
 router.put("/unfollow",authMiddleware,unfollowUserCtrl);
 router.put("/block-user/:id",authMiddleware,blockUserCtrl);
+router.put("/unblock-user/:id",authMiddleware,unBlockUserCtrl);
 router.get("/profile/:id", authMiddleware, userProfileCtrl);
 router.put("/:id", authMiddleware, updateUserProfileCtrl);
 router.delete('/:id', deleteUserCtrl);
