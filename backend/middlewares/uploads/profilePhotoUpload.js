14d9f706e6edac7a3b26b4a23ca3d3de55b04a25
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2022-08-20 23:54:12 +0100

    profile photo upload

--- backend/middlewares/uploads/profilePhotoUpload.js
+++ backend/middlewares/uploads/profilePhotoUpload.js
@@ -1,12 +1,13 @@
 const multer = require("multer");
-
+const sharp = require("sharp")
+const path = require("path")
 //storage
 const multerStorage = multer.memoryStorage();
 
 //file type checking
 const multerFilter = (req, file, cb) =>{
     // check file type
-    if (file.mimeType.startsWith("image")){
+    if (file.mimetype.startsWith("image")){
         cb(null, true)
     }else {
         //rejected files
@@ -22,8 +23,23 @@ const multerFilter = (req, file, cb) =>{
 const profilePohotoUpload = multer({
     storage: multerStorage,
     fileFilter: multerFilter,
+    limits:{fileSize:1000000},
 });
 
+
+//Image Resizing
+const profilePhotoResize = async(req , res, next) => {
+// check if there is no file 
+if(!req.file) return next();
+    req.file.filename = `user-${Date.now()}-${req.file.originalname}`
+    await sharp(req.file.buffer)
+    .resize(250, 250)
+    .toFormat("jpeg")
+    .jpeg({quality: 90})
+    .toFile(path.join(`public/images/profile/${req.file.filename}`))
+    next();
+}
 module.exports = {
-    profilePohotoUpload
+    profilePohotoUpload,
+    profilePhotoResize
 }
